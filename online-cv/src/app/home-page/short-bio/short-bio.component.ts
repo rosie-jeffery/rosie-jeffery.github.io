c07277dbf891ec2548f50c3d1fd859839cad3a8a
rosie-c <rosiecjay@gmail.com>
2024-08-13 22:38:52 +0100

    Created homepage and added some basic tailwind styling. Got slightly caried away with the styling. Added transition easing and some basic responsive design to the homepage.

---
+++ online-cv/src/app/home-page/short-bio/short-bio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-bio',
+  standalone: true,
+  imports: [],
+  templateUrl: './short-bio.component.html',
+  styleUrl: './short-bio.component.css'
+})
+export class ShortBioComponent {
+
+}
